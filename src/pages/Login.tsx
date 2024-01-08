9293ca8ade1f90cf96e83b1fceb2866fc3d2d496
alkmn <alkmn99@gmail.com>
2024-01-08 13:52:40 +0700

    fix: resolve choose login error  and add google oauth login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,38 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { Link, NavigateFunction, useNavigate } from "react-router-dom";
+import { CredentialResponse, GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+const timeOutMessage: number = 2000;
 
 interface UserProps {
   email: string
-  phone: string
   password: string
 }
 
 interface UserRequest {
   email: string
-  phone: string
   password: string
 }
 
 export default function Login() {
   const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [successMessage, setSuccessMessage] = useState<string>("");
   const [failMessage, setFailMessage] = useState<string>("");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [chooseLogin, setChooseLogin] = useState<boolean>(true);
   const [user, setUser] = useState<UserProps>(
     {
-      email: "", // atuny0@sohu.com,
-      phone: "", // +637916758914
+      email: "", // atuny0@sohu.com
       password: "" // 9uQFF1Lh
     }
   );
+  const navigate: NavigateFunction = useNavigate()
 
   useEffect(() => {
     fetch("https://dummyjson.com/users/1")
       .then((res) => res.json())
-      .then((data) => setUser({ email: data.email, phone: data.phone.split(" ").join(""), password: data.password }))
+      .then((data) => setUser({ email: data.email, password: data.password }))
   }, []);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,9 +38,6 @@ export default function Login() {
     switch (name) {
       case "email":
         setEmail(value);
-        break;
-      case "phone":
-        setPhone(value);
         break;
       case "password":
         setPassword(value);
@@ -53,42 +48,30 @@ export default function Login() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     setIsLoading(true);
     event.preventDefault();
-    const timeOutMessage: number = 2000;
     try {
       const payload: UserRequest = {
         email,
-        phone,
         password
       }
-
-      if (chooseLogin) {
-        if (!payload.email || !payload.password) {
-          throw new Error(`${!payload.email ? "Email" : "Password"} tidak boleh kosong!`);
-        }
-
-        if (!payload.email.includes("@")) {
-          throw new Error("Format email salah!");
-        }
-
-        if (payload.email !== user.email) {
-          throw new Error("Email tidak sesuai!");
-        }
-
-        if (payload.password !== user.password) {
-          throw new Error("Password tidak sesuai!");
-        }
-      } else {
-        if (!payload.phone) {
-          throw new Error("Nomor HP tidak boleh kosong!");
-        }
-
-        if (payload.phone !== user.phone) {
-          throw new Error("Nomor HP tidak sesuai!");
-        }
+      if (!payload.email || !payload.password) {
+        throw new Error(`${!payload.email ? "Email" : "Password"} tidak boleh kosong!`);
+      }
+      if (!payload.email.includes("@")) {
+        throw new Error("Format email salah!");
+      }
+      if (payload.password.length < 8) {
+        throw new Error("Password minimal 8 huruf!")
+      }
+      if (payload.email !== user.email) {
+        throw new Error("Email tidak sesuai!");
+      }
+      if (payload.password !== user.password) {
+        throw new Error("Password tidak sesuai!");
       }
       setSuccessMessage("Login berhasil");
       setTimeout(() => {
         setSuccessMessage("");
+        navigate("/");
       }, timeOutMessage);
     } catch (error) {
       if (error instanceof Error) {
@@ -102,6 +85,29 @@ export default function Login() {
         setIsLoading(false);
       }, timeOutMessage);
     }
+  }
+
+  const handleCredentialResponse = async (credentialResponse: CredentialResponse) => {
+    try {
+      const bearerToken: string = 'Bearer'
+      const credentialToken = `${bearerToken} ${credentialResponse.credential}` as string
+      if (!credentialToken.startsWith(bearerToken)) {
+        throw new Error("Format token salah!")
+      }
+      setSuccessMessage("Login berhasil");
+      setTimeout(() => {
+        setSuccessMessage("");
+        navigate("/");
+      }, timeOutMessage);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message)
+      }
+    }
+  }
+
+  const handleCredentialResponseError = async () => {
+    console.log("Login gagal!")
   }
 
   return (
@@ -126,31 +132,17 @@ export default function Login() {
                 <div className="text-right text-red-400 hover:underline hover:text-red-900 mb-8">
                   <a href="#">Lupa Password?</a>
                 </div>
-                <button type="submit" className={`flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${!phone || !email || !password || isLoading ? "cursor-not-allowed" : ""}`} disabled={!phone || !email || !password || isLoading}>{isLoading ? "Loading ..." : "Log in"}</button>
+                <button type="submit" className={`flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${!email || !password || isLoading ? "cursor-not-allowed" : ""}`} disabled={!email || !password || isLoading}>{isLoading ? "Loading ..." : "Log in"}</button>
               </>
             </form>
             <p className="mt-5 text-center">Atau Log in dengan</p>
             <div className="mt-5 flex justify-center items-center">
-              {/* This button will replace with OAuth */}
-              <button type="button" className="w-23 bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold px-4 py-2">
-                <div className="flex items-center justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="w-6 h-6" viewBox="0 0 48 48">
-                    <defs>
-                      <path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
-                    </defs>
-                    <clipPath id="b">
-                      <use xlinkHref="#a" overflow="visible" />
-                    </clipPath>
-                    <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-                    <path clipPath="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
-                    <path clipPath="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
-                    <path clipPath="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
-                  </svg>
-                </div>
-              </button>
+            <GoogleOAuthProvider clientId="2182170302-4qed8hhs52i94pq1bob86itln3vj01f3.apps.googleusercontent.com">
+              <GoogleLogin onSuccess={handleCredentialResponse} onError={handleCredentialResponseError} shape="circle" type="standard" text="signin_with"/>
+            </GoogleOAuthProvider>
             </div>
             <p className="mt-5 text-center">Belum Punya Akun?</p>
-            <p className="mt-5 text-center"><a href="#" className="text-indigo-600 hover:text-indigo-500">Daftar</a> sekarang untuk pengalaman pemesanan yang lebih baik!</p>
+            <p className="mt-5 text-center"><Link to={'/register'} className="text-indigo-600 hover:text-indigo-500">Daftar</Link> sekarang untuk pengalaman pemesanan yang lebih baik!</p>
           </div>
         </div>
       </div>
